7df2e87a4b053fc802a61ccd6c41d7581f2f1f8c
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2021-04-16 01:12:38 +0700

    [62130500051][Fixed] value in Room.vue

--- src/components/util/Room.vue
+++ src/components/util/Room.vue
@@ -1,11 +1,12 @@
 <template>
-  <router-link :to="'/rooms/' + id" class="bg-black flex text-white my-5 p-4 text-xl">
+  <router-link :to="{ name: 'Room', params: { id: room.id } }" 
+    class="bg-black flex text-white my-5 p-4 text-xl">
       <div class="flex w-4/5 pl-5 items-center">
-        <span> {{ description }} </span>
+        <span> {{ room.description }} </span>
       </div>
       <div class="flex w-1/5 justify-around items-center">
-        <span>{{ numofperson }}</span>
-        <i @click="deleteRoom" class="fas fa-times text-red-500 text-2xl cursor-pointer"></i>
+        <span>{{ room.nameList.length }}</span>
+        <i @click.stop.prevent="deleteRoom" class="fas fa-times text-red-500 text-2xl cursor-pointer"></i>
       </div>
   </router-link>
 </template>
@@ -13,10 +14,10 @@
 <script>
 export default {
     name: 'Room',
-    props: ['description','numofperson','id'],
+    props: ['room'],
     methods: {
         deleteRoom(){
-            this.$emit('deleteroom', { roomId: this.id });
+            this.$emit('deleteroom', { roomId: this.room.id });
         }
     }
 }
